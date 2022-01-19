81d39f61d1579afe0b48bfbd1d70de8eab32e6f2
viktorvoltz <vctrchinyeaka@gmail.com>
2022-01-19 08:50:48 +0100

    upload function

---
+++ lib/provider/classDataProvider.dart
@@ -0,0 +1,17 @@
+
+class ClassData {
+  final String publicId;
+  final String resourceType;
+  final String createdAt;
+  final String secureUrl;
+  final String originalFilename;
+
+  ClassData({
+    required this.publicId,
+    required this.resourceType,
+    required this.createdAt,
+    required this.secureUrl,
+    required this.originalFilename,
+  });
+
+}
